2eb48f70275d137c7d5550978dedf4f384128775
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-10 20:20:39 +0200

    forums table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_172515) do
+ActiveRecord::Schema.define(version: 2020_04_10_181750) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "forums", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_forums_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_03_02_172515) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "forums", "users"
 end
