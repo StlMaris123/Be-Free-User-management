3d2b2c9f39122c176a7576ac8fcd50feafc9bdfc
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-27 13:45:57 +0200

    add link to notifications service

---
+++ credentials.rb
@@ -0,0 +1,15 @@
+Host
+ec2-34-234-228-127.compute-1.amazonaws.com
+Database
+d4dvgnde7566iv
+
+User
+uzpxrcblxcujcz
+Port
+5432
+Password
+a2f5e8cc10a0cc8af27ebeccaffc1378b2c7d7ff4b8e85c6e702f01e023b07aa
+URI
+postgres://uzpxrcblxcujcz:a2f5e8cc10a0cc8af27ebeccaffc1378b2c7d7ff4b8e85c6e702f01e023b07aa@ec2-34-234-228-127.compute-1.amazonaws.com:5432/d4dvgnde7566iv
+Heroku CLI
+heroku pg:psql postgresql-aerodynamic-05454 --app vast-escarpment-47667
