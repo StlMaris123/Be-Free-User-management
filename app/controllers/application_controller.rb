4852f3ea86851377b86b10e1c9b12a9f1ac89ba6
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-27 14:49:22 +0200

    quick fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # protect_from_forgery prepend: true
+  skip_before_filter :verify_authenticity_token
+  protect_from_forgery prepend: true, with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
