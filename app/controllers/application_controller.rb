e9f494a23b772d58255d83085e14655f1520f4fd
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-30 22:12:30 +0200

    skip authenticity tokens for json requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, prepend: true
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token, if: :json_request?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
   protected
+  def json_request?
+    request.format.json?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[phone_number first_name
