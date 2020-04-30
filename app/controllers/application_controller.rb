e8f2f271583167485b2a1a3e4748dbba944d2d1f
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-30 20:57:07 +0200

    CRF token not verified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # skip_before_action :verify_authenticity_token
-  protect_from_forgery prepend: true, with: :null_session
+  protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
