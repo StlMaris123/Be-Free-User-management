630af2fa201d93ea62eb0b1ac7bc38ed2c4345dd
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-11 15:06:30 +0200

    use actual domain name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,7 @@
 Rails.application.config.session_store :redis_session_store, {
   key: "_be_free_session_#{Rails.env}",
   serializer: :json,
-  domain: :all,
-  tld_length: 2,
+  domain: 'vast-escarpment-47667',
   redis: {
     expire_after: 1.week,
     key_prefix: 'be_free:session',
